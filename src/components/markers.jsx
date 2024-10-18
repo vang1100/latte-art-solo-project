912c1e0d72c5279a30f328452555b829812d237e
Padee Vang <vangpadee11@gmail.com>
2024-10-18 15:35:55 -0500

    added some markers

---
+++ src/components/markers.jsx
@@ -0,0 +1,44 @@
+const markers = [
+{
+    position:{lat: 44.95152716625376, lng: -93.08509290488523},
+    name:"Eggdonut's Cafe",
+    description: "289 5th St E, St Paul, MN 55101",
+},
+
+{
+    position: {lat: 44.93331013518692, lng: -93.16571733665351},
+    name:"Spyhouse Coffee Roasters",
+    description: "420 Snelling Ave S, St Paul, MN 55105"
+},
+
+{
+    position: {lat: 4.93792786603352, lng: -93.08503648979277},
+    name: "Backstory Coffee Roasters",
+    description: "432 Wabasha St S, St Paul, MN 55107"
+},
+
+{
+    position: {lat: 4.9708932535762, lng: -93.07287812408573},
+    name: "Caydence Records & Coffee",
+    description: "900 Payne Ave, St Paul, MN 55130"
+},
+
+{
+    position: {lat: 44.94784451659447, lng: -93.14976096207641},
+    name: "Yellobird Coffee Bar",
+    description: "1168 Selby Ave, St Paul, MN 55104"
+},
+
+ {
+    position: {lat: 44.935571311797865, lng: -93.16622019265698},
+    name: "Roots Roasting",
+    description: "1552 St Clair Ave, St Paul, MN 55105"
+ },
+
+{
+    position: {lat: 44.96072030599185, lng: -93.15686464757776},
+    name: "Groundswell",
+    description: "1340 Thomas Ave W, St Paul, MN 55104"
+}
+
+];
