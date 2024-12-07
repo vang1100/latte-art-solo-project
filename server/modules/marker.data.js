9f7c90b53907b754ef8d258aab7972344eed4ac7
Padee Vang <vangpadee11@gmail.com>
2024-12-07 11:23:57 -0600

    moved markers to module folder

---
+++ server/modules/marker.data.js
@@ -0,0 +1,53 @@
+const markers = [
+    {
+        position:{lat: 44.95152716625376, lng: -93.08509290488523},
+        image:"eggdonut.jpg",
+        name:"Eggdonut's Cafe",
+        description: "289 5th St E, St Paul, MN 55101"
+    },
+    
+    {
+        position: {lat: 44.93331013518692, lng: -93.16571733665351},
+        image:"spyhouse.jpg",
+        name:"Spyhouse Coffee Roasters",
+        description: "420 Snelling Ave S, St Paul, MN 55105"
+    },
+    
+    {
+        position: {lat: 44.933053663043836, lng:-93.0841353032228},
+        image:"backstory.jpg",
+        name: "Backstory Coffee Roasters",
+        description: "432 Wabasha St S, St Paul, MN 55107"
+    },
+    
+    {
+        position: {lat: 44.96851787468767, lng:-93.07338678131681},
+        image:"caydence.jpg",
+        name: "Caydence Records & Coffee",
+        description: "900 Payne Ave, St Paul, MN 55130"
+    },
+    
+    {
+        position: {lat: 44.94784451659447, lng: -93.14976096207641},
+        image:"yellowbird.jpg",
+        name: "Yellowbird Coffee Bar",
+        description: "1168 Selby Ave, St Paul, MN 55104"
+    },
+    
+     {
+        position: {lat: 44.935571311797865, lng: -93.16622019265698},
+        image:"roots.jpg",
+        name: "Roots Roasting",
+        description: "1552 St Clair Ave, St Paul, MN 55105"
+     },
+    
+    {
+        position: {lat: 44.96072030599185, lng: -93.15686464757776},
+        image:"groundswell.jpg",
+        name: "Groundswell",
+        description: "1340 Thomas Ave W, St Paul, MN 55104"
+    }
+    
+    ];
+
+export default markers;
